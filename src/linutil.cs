5c636401ba429da340dc8a51ef0bf28bd8bd9f94
Chris Titus <contact@christitus.com>
2024-07-10 16:43:39 -0500

    menu work

--- src/linutil.cs
+++ src/linutil.cs
@@ -1,24 +1,27 @@
 namespace LinUtil {
     using Terminal.Gui;
     using System;
+    using System.Diagnostics;
+    using System.IO;
 
     public partial class LinUtil {
-        
+        private View outputView = new View(); // Initialize here
+
         public LinUtil() {
             SetupMenu();
+            SetupOutputView();
         }
 
         private void SetupMenu() {
-
             var menu = new MenuBar(new MenuBarItem[] {
                 new MenuBarItem("_File", new MenuItem[] {
                     new MenuItem("_Setup Bash Prompt", "", () => {
-                        MessageBox.Query("Debug", "Setup Bash Prompt selected", "Ok");
-                        SetupBashPromptCommand();
+                        RunBashScript("https://raw.githubusercontent.com/ChrisTitusTech/mybash/main/setup.sh");
+                        Application.Refresh(); // Redraw the menu
                     }),
                     new MenuItem("_Setup Neovim", "", () => {
-                        MessageBox.Query("Debug", "Setup Neovim selected", "Ok");
-                        SetupNeovimCommand();
+                        RunBashScript("https://raw.githubusercontent.com/ChrisTitusTech/neovim/main/setup.sh");
+                        Application.Refresh(); // Redraw the menu
                     }),
                     new MenuItem("_Quit", "", () => {
                         QuitCommand();
@@ -29,18 +32,74 @@ namespace LinUtil {
             Application.Top.Add(menu);
         }
 
-        private void SetupBashPromptCommand() {
-            MessageBox.Query("Debug", "Setup Bash Prompt command invoked", "Ok");
-            // Implement the Setup Bash Prompt command logic here
+        private void SetupOutputView() {
+            outputView = new View {
+                X = 0,
+                Y = 2, // Leave space for the menu and window title
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            Application.Top.Add(outputView);
         }
 
-        private void SetupNeovimCommand() {
-            MessageBox.Query("Debug", "Setup Neovim command invoked", "Ok");
-            // Implement the Setup Neovim command logic here
+        private void RunBashScript(string scriptUrl) {
+            var tempScriptPath = Path.Combine(Path.GetTempPath(), "temp_script.sh");
+
+            var downloadProcess = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"curl -L {scriptUrl} -o {tempScriptPath} && chmod +x {tempScriptPath}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            downloadProcess.OutputDataReceived += (sender, args) => {
+                Application.MainLoop.Invoke(() => {
+                    if (args.Data != null) {
+                        var label = new Label(args.Data) {
+                            X = 0,
+                            Y = outputView.Subviews.Count // Position below the last added view
+                        };
+                        outputView.Add(label);
+                    }
+                });
+            };
+
+            downloadProcess.Start();
+            downloadProcess.BeginOutputReadLine();
+            downloadProcess.WaitForExit();
+
+            var executeProcess = new Process {
+                StartInfo = new ProcessStartInfo {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{tempScriptPath}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            executeProcess.OutputDataReceived += (sender, args) => {
+                Application.MainLoop.Invoke(() => {
+                    if (args.Data != null) {
+                        var label = new Label(args.Data) {
+                            X = 0,
+                            Y = outputView.Subviews.Count // Position below the last added view
+                        };
+                        outputView.Add(label);
+                    }
+                });
+            };
+
+            executeProcess.Start();
+            executeProcess.BeginOutputReadLine();
+            executeProcess.WaitForExit();
         }
 
         private void QuitCommand() {
-            MessageBox.Query("Debug", "Quit command invoked", "Ok");
+            Console.Clear(); // Clear the terminal screen
             Application.RequestStop();
         }
 
--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15e9c8ebe46f845ba1d2685dc5de38d17427441f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bac8b79a9827eafb8567019602074284dde92c8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
